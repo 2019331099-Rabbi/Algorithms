9aa156afc7821049e8883c133e321efcf2a79655
Fazla Rabbi <fazla99@student.sust.edu>
2022-07-02 00:34:38 +0600

    DP with string

--- Dynamic Programming/DP With String/TotalPalindromicSubsequence.cpp
+++ Dynamic Programming/DP With String/TotalPalindromicSubsequence.cpp
@@ -1,59 +1,55 @@
 #include <bits/stdc++.h>
 #define endl '\n'
 #define PI acos(-1)
-#define sz 1000
+#define sz 61
 #define inf 1e9 + 9
 #define mod 1000000007
 #define RUN_FAST ios::sync_with_stdio(false);
 using namespace std;
 
 /**
-********** Problem 8 ************
-You have given a string. Find the longest substring that is a palindrome.
+You are given a string. Find how many palindromic subsequence can be formed from the given string.
+For example
+s = PASTA
+8 palindromic subsequence can be formed
+P, A, S, T, A, AA, ASA, ATA
 
-Test Case:
-abaab
-Output: 3
+SALADS
+15
 
-abbaeae
-Output: 4
+For more details visit https://www.geeksforgeeks.org/count-palindromic-subsequence-given-string/
 */
 
-
-int memo[sz][sz];
+int64_t memo[sz][sz];
+int len;
 string s;
+
+int64_t dp(int left, int right)
+{
+    if (left == right) return 1;
+    if (left > right) return 0;
+    if (memo[left][right] != -1) return memo[left][right];
+
+    int64_t val;
+    if (s[left] == s[right]) val = 1 + dp(left + 1, right) + dp(left, right - 1);
+    //Checking the rest once respect to left and once respect to right
+    else val = dp(left + 1, right) + dp(left, right - 1) - dp(left + 1, right - 1);
+    //Checking the rest and removing the common parts.
+    return memo[left][right] = val;
+}
 
 void init()
 {
     memset(memo, -1, sizeof(memo));
 }
 
-int isPalindrome(int l, int r)
-{
-    if (l >= r) return 1;
-    if (memo[l][r] != -1) return memo[l][r];
-
-    if (s[l] == s[r]) return memo[l][r] = isPalindrome(l + 1, r - 1);
-    else return memo[l][r] = 0;
-}
-
-int TotalPalindromicSubstring()
-{
-    int len = s.size(), ans = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; j++) if (isPalindrome(i, j)) ans++;
-    }
-    return ans;
-}
-
 int main()
 {
     RUN_FAST; cin.tie(nullptr);
-    init();
-
     cin >> s;
-    int ans = TotalPalindromicSubstring();
-    cout << "Total palindromic substring = " << ans << endl;
+    len = s.size();
+    init();
+    cout <<  dp(0, len - 1) << endl;
 
     return 0;
 }
--- Dynamic Programming/DP With String/TotalPalindromicSubstring.cpp
+++ Dynamic Programming/DP With String/TotalPalindromicSubstring.cpp
@@ -1,55 +1,59 @@
 #include <bits/stdc++.h>
 #define endl '\n'
 #define PI acos(-1)
-#define sz 61
+#define sz 1000
 #define inf 1e9 + 9
 #define mod 1000000007
 #define RUN_FAST ios::sync_with_stdio(false);
 using namespace std;
 
 /**
-You are given a string. Find how many palindromic subsequence can be formed from the given string.
-For example
-s = PASTA
-8 palindromic subsequence can be formed
-P, A, S, T, A, AA, ASA, ATA
+********** Problem 8 ************
+You have given a string. Find the longest substring that is a palindrome.
 
-SALADS
-15
+Test Case:
+abaab
+Output: 3
 
-For more details visit https://www.geeksforgeeks.org/count-palindromic-subsequence-given-string/
+abbaeae
+Output: 4
 */
 
-int64_t memo[sz][sz];
-int len;
+
+int memo[sz][sz];
 string s;
-
-int64_t dp(int left, int right)
-{
-    if (left == right) return 1;
-    if (left > right) return 0;
-    if (memo[left][right] != -1) return memo[left][right];
-
-    int64_t val;
-    if (s[left] == s[right]) val = 1 + dp(left + 1, right) + dp(left, right - 1);
-    //Checking the rest once respect to left and once respect to right
-    else val = dp(left + 1, right) + dp(left, right - 1) - dp(left + 1, right - 1);
-    //Checking the rest and removing the common parts.
-    return memo[left][right] = val;
-}
 
 void init()
 {
     memset(memo, -1, sizeof(memo));
 }
 
+int isPalindrome(int l, int r)
+{
+    if (l >= r) return 1;
+    if (memo[l][r] != -1) return memo[l][r];
+
+    if (s[l] == s[r]) return memo[l][r] = isPalindrome(l + 1, r - 1);
+    else return memo[l][r] = 0;
+}
+
+int TotalPalindromicSubstring()
+{
+    int len = s.size(), ans = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) if (isPalindrome(i, j)) ans++;
+    }
+    return ans;
+}
+
 int main()
 {
     RUN_FAST; cin.tie(nullptr);
-    cin >> s;
-    len = s.size();
     init();
-    cout <<  dp(0, len - 1) << endl;
+
+    cin >> s;
+    int ans = TotalPalindromicSubstring();
+    cout << "Total palindromic substring = " << ans << endl;
 
     return 0;
 }
